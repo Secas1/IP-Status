7e30d081af102369b7f4288c7a28317a16db11db
Secas1 <cut-me-open@hotmail.es>
2020-05-18 12:51:28 +0000

    IP Error Resolved

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 
 INPUT=RandomDataTestCSV.csv
 OLDIFS=$IFS
-IFS=','
+IFS=';'
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
 
 while read hostname ip
@@ -12,7 +12,7 @@ do
 	echo "Hostname: $hostname"
 	echo "================================="
 	echo "IP: $ip"
-	ping -c $ip
+	ping -c 1 $IP_TEST
 	if [[ $? == 0 ]]; then
 		echo "Ping Success"
 	else  
