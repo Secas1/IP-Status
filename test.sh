44cdb46f0c714377a83f4cc6aeb5e7bac992c636
Secas1 <cut-me-open@hotmail.es>
2020-05-18 13:36:47 +0000

    Update

--- test.sh
+++ test.sh
@@ -6,21 +6,24 @@ OLDIFS=$IFS
 IFS=';'
 COUNT_SUCCESS=0
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-
+echo "hostname;ip;connection_status" >> status.csv
 while read hostname ip
 do
 	echo "================================="
 	echo "Hostname: $hostname"
 	echo "================================="
 	echo "IP: $ip"
-	ping -c 1 $ip
+	ping -c 2 $ip
+
 	if [[ $? == 0 ]]; then
 		echo "Ping Success"
+		echo "$hostname;$ip;success"
 		let "COUNT_SUCCESS+=1"
 	else  
 		echo "Ping Fail"
+		echo "$hostname;$ip;fail"
 	fi
-	echo ""
+	echo " "
 done < $INPUT
 
 echo "Connection established: $COUNT_SUCCESS"
